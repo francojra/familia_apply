a40308d436fbee6d22738b0255895404140d8973
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-21 23:44:18 -0300

    Add files via upload

--- familia_apply.R
+++ familia_apply.R
@@ -21,11 +21,16 @@ starwars
 
 # Função apply -----------------------------------------------------------------------------------------------------------------------------
 
+## Aplica uma função a um vetor ou matriz a partir de uma margem especificada.
+
 starwars %>%
   select(height, mass, birth_year) %>%
   apply(MARGIN = 2, FUN = sum, na.rm = T)
 
 # Função lapply ----------------------------------------------------------------------------------------------------------------------------
+
+## Similar ao apply, a diferença é que poderá receber uma lista, um vetor,
+## ou um data frame como entrada.
 
 starwars %>%
   select(height, mass, birth_year) %>%
@@ -33,11 +38,17 @@ starwars %>%
 
 # Função sapply ----------------------------------------------------------------------------------------------------------------------------
 
+## Similar ao lapply, a diferença é que o resultado é apresentado em um formato
+## mais amigável.
+
 starwars %>%
   select(height, mass, birth_year) %>%
   sapply(MARGIN = 2, FUN = median, na.rm = T)
 
 # Função tapply ----------------------------------------------------------------------------------------------------------------------------
+
+## Aplica uma função a um subconjunto de um vetor que é construído a partir
+## de um outro vetor (normalmente, um fator).
 
 tapply(starwars$height, starwars$eye_color, 
        FUN = mean, na.rm = T)
